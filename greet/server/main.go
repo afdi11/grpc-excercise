ca4a726922cc5b8e8b5826e598be78fe01e47ea0
Afdi <45322055+afdi11@users.noreply.github.com>
2023-01-16 17:15:59 +0700

    Say Greet

---
+++ greet/server/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	pb "github.com/afdi11/grpc-excercise/greet/proto"
+	"google.golang.org/grpc"
+)
+
+var addr string = "0.0.0.0:50051"
+
+type Server struct {
+	pb.GreetServiceServer
+}
+
+func main() {
+	lis, err := net.Listen("tcp", addr)
+
+	log.Printf("Listening on %s\n", addr)
+
+	s := grpc.NewServer()
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
